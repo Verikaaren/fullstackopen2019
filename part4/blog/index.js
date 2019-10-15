942ab2158846b1d770096d9dda0e7afbe0f31f59
Andrei <Andrei.redi@gmail.com>
2019-10-15 10:59:48 +0300

    new blog app

---
+++ part4/blog/index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const blogSchema = mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const url =
+	'mongodb+srv://Bulgur_Ghost:yuLqezjrHpfongjogrQPldCOOOIQgvTyzicQTFGGodCAHiOmFNkeGnkQWifzHVzwefQaQ@cluster0-tlf7e.mongodb.net/blog?retryWrites=true&w=majority';
+
+
+mongoose
+	.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+	
+	.then(result => {
+		console.log('connected to MongoDB');
+	})
+	.catch(error => {
+		console.log('error connecting to MongoDB:', error.message);
+	});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/api/blogs', (request, response) => {
+	Blog.find({}).then(blogs => {
+		response.json(blogs);
+	});
+});
+
+app.post('/api/blogs', (request, response) => {
+	const blog = new Blog(request.body);
+
+	blog.save().then(result => {
+		response.status(201).json(result);
+	});
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
